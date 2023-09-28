81eda9b14345a7973f82b52b6b9d2e28c7303040
Mahdi Safarmohammadloo <mahdisml98@gmail.com>
2023-09-29 02:40:46 +0330

    Create fps_label.gd

---
+++ fps_label.gd
@@ -0,0 +1,18 @@
+extends Label
+
+var exact_fps : float
+var fps_count : int
+
+func _ready():
+	pass
+
+func _process(delta):
+	exact_fps += (1 / delta);
+	fps_count += 1
+	
+	if (fps_count >= 60):
+		exact_fps = (exact_fps / 60)
+		self.text = str(exact_fps)
+		exact_fps = 0
+		fps_count = 0
+	pass
